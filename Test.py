7868f215ef85bfa3313adc3c90e1b80ee7aed1a6
Tony <fanguangjun>
2019-11-25 17:34:46 +0800

    Signed-off-by: Tony <fanguangjun>

---
+++ Test.py
@@ -0,0 +1,19 @@
+print("aaaaaaaaaaaaaa")
+
+counter = 100  # 整型变量
+miles = 1000.0  # 浮点型变量
+name = "runoob"  # 字符串
+
+print(counter)
+print(miles)
+print(name)
+
+str = 'Runoob'
+
+print(str)  # 输出字符串
+print(str[0:-1])  # 输出第一个到倒数第二个的所有字符
+print(str[0])  # 输出字符串第一个字符
+print(str[2:5])  # 输出从第三个开始到第五个的字符
+print(str[2:])  # 输出从第三个开始的后的所有字符
+print(str * 2)  # 输出字符串两次
+print(str + "TEST")  # 连接字符串
